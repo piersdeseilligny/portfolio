e644c618fce47fc3d2c2cef2a0687a4cc64ff61a
Piers Deseilligny <42496508+piersdeseilligny@users.noreply.github.com>
2022-01-26 00:19:06 +0000

    Update sitemap

--- nuxt/modules/sitemapRouteGenerator.js
+++ nuxt/modules/sitemapRouteGenerator.js
@@ -2,7 +2,14 @@ export default function () {
     this.nuxt.hook('generate:done', (context) => {
       const routesToExclude = [] // Add any route you don't want in your sitemap. Potentially get this from an .env file.
       const allRoutes = Array.from(context.generatedRoutes)
-      const routes = allRoutes.filter(route => !routesToExclude.includes(route))
+      let routes = allRoutes.filter(route => !routesToExclude.includes(route))
+
+      routes = routes.map(function(val){
+        if(!val.endsWith('/')){
+           return val + '/'
+        }
+        else return val;
+      });
   
       this.nuxt.options.sitemap.routes = [...routes]
     })
